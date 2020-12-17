4c9fa6040e57fa15767d5eb3456a76cd919f136a
Mahak Makharia <makhariamahak@gmail.com>
2020-12-17 14:03:39 +0530

    (add) : basic structure of actions and reducers

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 console.log("works fine");
+const BUY_CAKE = "BUY_CAKE";
+
+buyCake = () => {
+  return {
+    type: BUY_CAKE,
+    info: "customer buys a cake",
+  };
+};
+
+const initialState = {
+  noOfCakes: 10,
+};
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case BUY_CAKE:
+      return {
+        ...state,
+        noOfCakes: state.noOfCakes - 1,
+      };
+    default:
+      return state;
+  }
+};

0882e06cf6dc84103c691cbaf153cb97b1e44054
Mahak Makharia <makhariamahak@gmail.com>
2020-12-19 12:30:19 +0530

    (add) : redux to cake store example

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
-console.log("works fine");
+const redux = require("redux");
+const createStore = redux.createStore;
+
 const BUY_CAKE = "BUY_CAKE";
 
 buyCake = () => {
@@ -23,3 +25,13 @@ const reducer = (state = initialState, action) => {
       return state;
   }
 };
+
+const store = createStore(reducer);
+console.log("Initial state", store.getState());
+const unsusbcribe = store.subscribe(() => {
+  console.log("Updated State", store.getState());
+});
+store.dispatch(buyCake());
+store.dispatch(buyCake());
+store.dispatch(buyCake());
+unsusbcribe();
